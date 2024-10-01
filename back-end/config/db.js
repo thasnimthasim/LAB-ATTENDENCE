a20de6ee1b77ba3162fb22a1bc8a1d26e1d44973
Thasnim <thasnimthasim625@gmail.com>
2024-10-01 16:12:49 +0200

    Update frontend

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 mongoose
-        .connect("mongodb://localhost:27017/attendanceRegister")
+        .connect("mongodb+srv://thasnim:thasnim123@cluster0.hvn7j.mongodb.net/lab")
         .then(()=>{
                     console.log("connected to mongodb");
                     
